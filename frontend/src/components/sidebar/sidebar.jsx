90276e7e4b33e342ed3b4ccd92cecf120cf4ea8c
Lonnie P <allon.paz@gmail.com>
2019-08-02 14:10:51 -0700

    created sidebar shit

--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -1,39 +1,102 @@
 import React from 'react';
-import {Component} from 'react';
-import '../../App.css'
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-class App extends Component{
-
-    state = {
-        name: null,
-        password: null,
-        email: null
-    }
-
-
-titsandass = (name, password, email) => {
-axios.post('http://localhost:8080/api/accounts/signUp', 
-{
-name: name,
-password: password,
-email: email
-}).then(()=>console.log('hello'))}
-  render (){
-    const form = {
-      width: '50%',
-      backgroundColor: 'black',
-      color: 'white',
-      justifySelf: 'center'
-    }
   return (
-    <div style={form}>
-        <input id='nameinp' type="text" placeholder="name" onChange={(e) => this.setState({ name: e.target.value })}></input>
-        <input id='passinp' type="text" placeholder="password" onChange={(e) => this.setState({ password: e.target.value })}></input>
-        <input id='emailinp' type="text" placeholder="email" onChange={(e) => this.setState({ email: e.target.value })}></input>
-        <button onClick={() => this.titsandass(this.state.name, this.state.password, this.state.email)}>push</button>
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export default function VerticalTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab label="Item One" {...a11yProps(0)} />
+        <Tab label="Item Two" {...a11yProps(1)} />
+        <Tab label="Item Three" {...a11yProps(2)} />
+        <Tab label="Item Four" {...a11yProps(3)} />
+        <Tab label="Item Five" {...a11yProps(4)} />
+        <Tab label="Item Six" {...a11yProps(5)} />
+        <Tab label="Item Seven" {...a11yProps(6)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Item Six
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        Item Seven
+      </TabPanel>
     </div>
   );
-}}
+}
 
 export default App;
