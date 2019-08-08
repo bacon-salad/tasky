076d375bcae57ef30a4e9470b5bfcd101d6114b9
Devon <Dvowen@cox.net>
2019-08-07 19:29:24 -0700

    fixing tables

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
